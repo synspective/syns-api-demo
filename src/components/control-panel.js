eeb44db889251dab1b5bd46dfeeded3848d1c065
Daryl Cole <darylcoleoffice@gmail.com>
2021-10-12 13:16:09 +0900

    Break Break panel into separate styled component

--- src/components/control-panel.js
+++ src/components/control-panel.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const ControlPanelWrapper = styled.div`
-	position: absolute;
-	top: 0;
-	right: 0;
-	max-width: 320px;
-	background: #fff;
-	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
-	padding: 12px 24px;
-	margin: 20px;
-	font-size: 13px;
-	line-height: 2;
-	color: #6b6b76;
-	outline: none;
-`;
+import Panel from './styled-components/panel.styled';
 
 const ControlPanel = ({ children }) => {
 	return (
-		<ControlPanelWrapper>
+		<Panel>
 			<h3>Synspective API Demo</h3>
 			<p>Something something blah blah</p>
 			{children}
-		</ControlPanelWrapper>
+		</Panel>
 	);
 };
 
---
+++ src/components/styled-components/panel.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Panel = styled.div`
+	max-width: 320px;
+	background: #fff;
+	box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
+	padding: 12px 24px;
+	font-size: 13px;
+	line-height: 2;
+	color: #6b6b76;
+	outline: none;
+`;
+
+export default Panel;
