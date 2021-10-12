2e3d3f2fc1741a0ce703eb53fc89af84f77e20d1
Daryl Cole <darylcoleoffice@gmail.com>
2021-10-12 13:16:29 +0900

    Add panel section

---
+++ src/components/styled-components/panel-section.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const PanelSection = styled.div`
+	position: absolute;
+	top: 0;
+	right: 0;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	padding: 20px;
+`;
+
+export default PanelSection;
